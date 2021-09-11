a6d3a43a9c6910c79fe4b382a6cd2aabf9e4bbdf
Luis Chilo <gfxargentina@gmail.com>
2021-09-11 11:58:25 -0300

    basic http server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log("hello prueba");
+import { startServer } from "./server";
+
+async function main() {
+  const port: number = 4000;
+  const app = await startServer();
+  app.listen(port);
+  console.log("El servidor esta corriendo en el puerto", port);
+}
+
+main();
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+//inicia el servidor
+
+export async function startServer() {
+  const app = express();
+  return app;
+}
