9208ed616ed1eb5bc6a55aa6addfe6c8a39d65d8
Luis Chilo <gfxargentina@gmail.com>
2021-12-22 20:29:00 -0300

    add styles

--- bibliotecafrontend/pages/nuevacuenta.js
+++ bibliotecafrontend/pages/nuevacuenta.js
@@ -175,7 +175,7 @@ const NuevaCuenta = () => {
               </div>
               <input
                 type="submit"
-                className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
+                className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900 cursor-pointer"
                 value="Registrar nuevo usuario"
               />
             </form>
